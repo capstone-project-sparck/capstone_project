caa54dd25a635e88ac98879ea74fff2dd7f65f24
Jehp00 <jehp01104006@gmail.com>
2023-02-07 15:41:10 -0500

    connections db

---
+++ models/insert_connections.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/config')
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/etl')
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/models')
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
+from config.db import session
+import numpy as np
+from etl.affinity_data_structuring import connections_data_rearranged_final
+from models.consolidate import Connection
+
+connections_data = connections_data_rearranged_final.replace(np.nan, 'empty')
+connections_to_dict = connections_data.to_dict("records")
+
+
+for row in connections_to_dict:
+    connection = Connection(**row)
+    session.add(connection)
+    session.commit()
--- models/insert_investors_db.py
+++ models/insert_investors_db.py
@@ -2,19 +2,20 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/config')
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/etl')
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + '/models')
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
-from uuid import uuid4
-from config.db import conn
+from config.db import session
 import numpy as np
 from etl.investors_data_structuring import investors_data
-from consolidate import consolidated
+from models.consolidate import Consolidated
+
 
 investors_data = investors_data.replace(np.nan, 'empty')
 investors_to_dict = investors_data.to_dict("records")
 
-for row in investors_to_dict:
-    row['id'] = str(uuid4())
-    conn.execute(consolidated.insert().values(row))
-    conn.commit()
 
+for row in investors_to_dict:
+    investor = Consolidated(**row)
+    session.add(investor)
+    session.commit()
 
