5f6a0a635ca3086091f484bfc5cba6da67d3176f
Jehp00 <jehp01104006@gmail.com>
2023-02-08 16:25:30 -0500

    fixing merge

--- app.py
+++ app.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-from fastapi import FastAPI
-from routes.consolidated_routes import investor_consolidated
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-# routes
-
-app.include_router(investor_consolidated)
-=======
 from fastapi import FastAPI
 from routes.consolidated_routes import investor_consolidated
 from routes.connection_routes import connection_consolidated
@@ -42,4 +20,3 @@ app.add_middleware(
 
 app.include_router(investor_consolidated)
 app.include_router(connection_consolidated)
->>>>>>> origin
