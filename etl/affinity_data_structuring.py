00b241cfda2f99993a78745b95e05e218153b309
Jehp00 <jehp01104006@gmail.com>
2023-02-09 14:35:24 -0500

    updating connection DO

--- etl/affinity_data_structuring.py
+++ etl/affinity_data_structuring.py
@@ -3,6 +3,53 @@ import pandas as pd
 import os
 import uuid
 
+def split_list(list_1):
+
+    result = []
+    if isinstance(list_1, list):
+        for elem in list_1:
+            elem_dict = {}
+
+            try:
+                name = elem.split(' <')[0]
+                email = elem.split(' <')[1]
+            except Exception:
+                continue
+            elem_dict['name'] = name
+            elem_dict['email'] = email
+            result.append(elem_dict)
+    return result
+
+
+def split_string(string_1):
+
+    elem_dict = {}
+
+    try:
+        name = string_1.split(' <')[0]
+        email = string_1.split(' <')[1]
+    except Exception:
+        return {}
+    elem_dict['name'] = name
+    elem_dict['email'] = email
+    return elem_dict
+
+
+def get_name(dict_1):
+    try:
+        result = dict_1['name']
+    except Exception:
+        return ''
+    return result
+
+
+def get_email(dict_1):
+    try:
+        result = dict_1['email']
+    except Exception:
+        return ''
+    return result
+
 data_route = os.path.abspath(
     os.path.join(
         os.getcwd(),
@@ -60,49 +107,3 @@ connections_data_rearranged_final = connections_data_rearranged_final[connection
     investors_data['id'])]
 
 
-def split_list(list_1):
-
-    result = []
-    if isinstance(list_1, list):
-        for elem in list_1:
-            elem_dict = {}
-
-            try:
-                name = elem.split(' <')[0]
-                email = elem.split(' <')[1]
-            except Exception:
-                continue
-            elem_dict['name'] = name
-            elem_dict['email'] = email
-            result.append(elem_dict)
-    return result
-
-
-def split_string(string_1):
-
-    elem_dict = {}
-
-    try:
-        name = string_1.split(' <')[0]
-        email = string_1.split(' <')[1]
-    except Exception:
-        return {}
-    elem_dict['name'] = name
-    elem_dict['email'] = email
-    return elem_dict
-
-
-def get_name(dict_1):
-    try:
-        result = dict_1['name']
-    except Exception:
-        return ''
-    return result
-
-
-def get_email(dict_1):
-    try:
-        result = dict_1['email']
-    except Exception:
-        return ''
-    return result
