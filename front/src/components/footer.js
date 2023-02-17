0114dfd32c98504a6e47286a1834d1d7a17eb1e6
alejandrodgz <5199@holbertonstudents.com>
2023-02-17 12:28:34 -0500

    front backup

--- front/src/components/footer.js
+++ front/src/components/footer.js
@@ -3,7 +3,7 @@ export default function Footer(){
     return(
         <div className="footerDiv">
         <footer style={{color:"gray", }}>
-            <small>@This is a project made by holbies from </small><img style={{maxWidth:"0.7rem"}} src={logoHol}/><span style={{color:"#E11D3F"}}><small> holberton school</small></span>
+            <small>@This is a project made by holbies from </small><img style={{maxWidth:"0.7rem"}} src={logoHol}/><span style={{color:"#E11D3F"}}><small>holberton school</small></span>
         </footer>
         </div>
     )
