f8a4e3de9a7773372b787c49ec8d144a5c0a4fd0
Jehp00 <jehp01104006@gmail.com>
2023-02-24 21:25:28 -0500

    update

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Login from "./login";
-import SingUp from "./SingUp"
+import SignUp from "./SignUp"
 import Filters from "./Filters";
 import Contact from "./Contact";
 import Protected from "./protected";
@@ -10,9 +10,12 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 export default function App(){
 
+  //states to test login and credentials
+
   let [loginPass, setLoginPass] = React.useState(false)
-  
   let [credentials, setCredentials] = React.useState({})
+
+  //this function is use to set the email and password
 
   function getCredentials(event){
       setCredentials((oldValue)=>{
@@ -27,13 +30,13 @@ export default function App(){
     <Route path="/" element={<Login setLoginPass={setLoginPass} loginPass={loginPass} 
         getCredentials={getCredentials} credentials={credentials}
         />} />
+        <Route path="/SignUp" element={ <SignUp getCredentials={getCredentials} credentials={credentials} 
+        setLoginPass={setLoginPass} loginPass={loginPass}/> }/>
         <Route element={<Protected loginPass={loginPass}/>}>
             <Route path="/Sources" element={<Filters className="filters" setLoginPass={setLoginPass} />} />
             <Route path="/Contact" element={<Contact setLoginPass={setLoginPass} />} />
             <Route path="/Connections" element={<Connections setLoginPass={setLoginPass} />} />
             {/*<Route path="/About" element={<About />} />*/}
-            <Route path="/SingUp" element={ <SingUp getCredentials={getCredentials} credentials={credentials} 
-            setLoginPass={setLoginPass} loginPass={loginPass}/> }/>
         </Route>
   </Routes>
   </BrowserRouter>
